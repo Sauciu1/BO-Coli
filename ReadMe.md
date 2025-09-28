e770f9734fef3f55e66639fa0126c663ab85bd46
Povilas Sauciuvienas <p.sauciuvienas@gmail.com>
2025-09-28 15:54:16 +0100

    hpc trial

--- ReadMe.md
+++ ReadMe.md
@@ -7,6 +7,7 @@ To run the project, assuming you have python 3.13 installed:
 cmd
 pip install poetry
 poetry lock
+poetry env activate
 ```
 
 You bo-coli venv should now appear, run everything from it.
--- h6_simulations.py
+++ h6_simulations.py
@@ -7,7 +7,9 @@ from botorch.models import SingleTaskGP
 from botorch.acquisition import qLogExpectedImprovement, qMaxValueEntropy
 import pickle
 import time
-
+import multiprocessing as mp
+import os
+from src.model_generation import GammaNoiseSGP, HeteroNoiseSGP, HeteroWhiteSGP
 
 param_range = [
     RangeParameterConfig(name=f"x{i+1}", parameter_type="float", bounds=(0.0, 1.0))
@@ -21,7 +23,7 @@ dim_names = [rp.name for rp in param_range]
 
 test_fn = Hartmann6D().eval_at
 
-from src.model_generation import GammaNoiseSGP, HeteroNoiseSGP, HeteroWhiteSGP
+
 
 def _single_run(params, n_runs = 30):
     tr, noise = params
@@ -42,37 +44,48 @@ def _single_run(params, n_runs = 30):
 
     return df_local
 
-save_dir = "data/bayes_sim/"
 
-import multiprocessing as mp
-import os
+
+
+
 
 print(__name__)
 mode = 'multicore'
-if __name__ == "__main__":
-    
+
+
+
+def run_grid(save_path ):
     n_runs = 100
     mp.set_start_method("spawn", force=True)  # Windows-safe
 
     t0 = time.perf_counter()
     print("Starting batch Bayesian optimization tests...")
 
-    param_grid = [(tr, float(n)) for tr in range(1, 9, 2) for n in np.linspace(0, 1.1, 6)]
-    n_workers = min(len(param_grid), os.cpu_count()-2 or 1)
+    #param_grid = [(tr, float(n)) for tr in range(1, 9, 2) for n in np.linspace(0, 1.1, 6)]
+    param_grid = [(tr, float(n)) for tr in range(1, 2) for n in np.linspace(0, 0.3, 2)]
+    n_workers = min(len(param_grid), os.cpu_count() or 1)
 
 
     with mp.get_context("spawn").Pool(processes=n_workers) as pool:
         results = pool.starmap(_single_run, [(el, n_runs) for el in param_grid])
 
-
     r_n_dict = dict(zip(param_grid, results))
 
-    with open(save_dir + 'singleGP_noise_vs_t_repeats_09_28.pkl', 'wb') as f:
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+        
+    with open(save_path, 'wb') as f:
         pickle.dump(r_n_dict, f)
 
     elapsed = time.perf_counter() - t0
     print(f"Completed {len(param_grid)} configs in {elapsed:.2f}s with {n_workers} workers")
     r_n_dict
 
+
+if __name__ == "__main__":
+    save_dir = "data/bayes_sim/"
+    save_PATH = save_dir + 'PBS_test_vs_t_repeats_09_28.pkl'
+
+    run_grid(save_PATH)
     # Runtime for sequential runs : 112s
     # Same code with 8 workers: 40s
